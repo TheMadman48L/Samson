d59d5319351fd8fc2605ea44707592d884858b14
Alexey Fabricius <themadman48l@gmail.com>
2018-11-17 02:02:48 +0300

    v 0.3

--- 2.php
+++ 2.php
@@ -17,3 +17,32 @@ function  convertString($a, $b)
     $a = substr_replace($a, $bReverse, $start, $end);
     return $a;
 }
+
+function mySortForKey($a, $b)
+{
+    foreach ($a as $k => $v) {
+        if (array_key_exists($b, $v)) {
+            $bArr[] = $v[$b];
+        } else {
+            throw new Exception("В подмассиве с индексом: {$k} нет элемента с индексом {$b}");
+        }
+    }
+
+    asort($bArr);
+
+    foreach ($bArr as $k => $v) {
+        $aRes[] = $a[$k];
+    }
+
+    return $aRes;
+}
+
+$a = [
+    ['a'=>2,'b'=>1, 'c'=>3],
+    ['a'=>1,'b'=>3, 'c'=>1],
+    ['a'=>4,'b'=>5, 'c'=>4],
+    ['a'=>7,'b'=>2, 'c'=>2],
+    ['a'=>2,'b'=>8, 'c'=>5],
+];
+$b = 'b';
+//var_dump(mySortForKey($a, $b));
