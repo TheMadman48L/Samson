ebb6846113eb96991b8b3c64fd989b528baa2c22
Alexey Fabricius <themadman48l@gmail.com>
2018-11-17 01:45:18 +0300

    v 0.2

--- 2.php
+++ 2.php
@@ -1,6 +1,19 @@
 <?php
 
+// так как функция strrev() не работает с кириллицей то нашел ее реализацию для мультибайтовых кодировок
+function mb_strrev($str){
+    $r = '';
+    for ($i = mb_strlen($str); $i>=0; $i--) {
+        $r .= mb_substr($str, $i, 1);
+    }
+    return $r;
+}
+
 function  convertString($a, $b)
 {
-
+    $bReverse = mb_strrev($b);
+    $start = mb_strpos($a, $b);
+    $end = mb_strlen($b);
+    $a = substr_replace($a, $bReverse, $start, $end);
+    return $a;
 }
